7395513e49f32b58fb8eec1a2ed84cc5425bcc34
IsaacAndreysd <isanchez50692@ufide.ac.cr>
2025-07-02 19:45:41 -0700

    commit prueba 2

--- Tienda_Andrey/src/main/java/Tienda/domain/Categoria.java
+++ Tienda_Andrey/src/main/java/Tienda/domain/Categoria.java
@@ -4,27 +4,31 @@
  */
 
 package Tienda.domain;
+
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
+
 @Data
 @Entity
-@Table(name="categoria")
-/**
- *
- * @author isaac
- */
+@Table(name = "categoria")
 public class Categoria implements Serializable {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id_categoria")
+    @Column(name = "id_categoria")
     private Long idCategoria;
+
     private String descripcion;
     private String rutaImagen;
     private boolean activo;
+
+    // ✅ Relación: una categoría tiene muchos productos
+    @OneToMany(mappedBy = "categoria")
+    private List<Producto> productos;
 
     public Categoria() {
     }
@@ -33,5 +37,13 @@ public class Categoria implements Serializable {
         this.descripcion = categoria;
         this.activo = activo;
     }
+
+    // ✅ Getter y Setter por si no usas Lombok bien
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
 }
- 
