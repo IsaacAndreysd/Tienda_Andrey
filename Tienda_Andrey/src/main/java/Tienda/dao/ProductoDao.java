f37a148d4cd59eff86867bc260fbee5bf53eb8cd
IsaacAndreysd <isanchez50692@ufide.ac.cr>
2025-07-02 19:10:52 -0700

    Commit semana 8 prueba
    prueba

---
+++ Tienda_Andrey/src/main/java/Tienda/dao/ProductoDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Tienda.dao;
+
+/**
+ *
+ * @author isaac
+ */
+
+import Tienda.domain.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoDao extends JpaRepository <Producto, Long> {
+    
+}
